1829d28c6a0c822cc0eb3d4969fda6da9641fc6f
Nizzy Niz <martingarvin@gmail.com>
2017-12-03 09:21:40 +0000

    no message
    Signed-off-by: Nizzy Niz <martingarvin@gmail.com>

--- hello.java
+++ hello.java
@@ -1,11 +1,8 @@
-public Class Hello{
+public class HelloWorld {
 
-			public static void main(String[] args){
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
 
-				for(int i=1; i<10; i++){
-
-					System.out.println("Hello world..." +i);
-
-				}
-			}
 }
